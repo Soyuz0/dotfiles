372c7049cde1186196d34d8f10b1c429698e26e8
RonLang <ron.lan888@gmail.com>
2025-06-28 22:21:30 +0300

    small fix to flash.nvim

--- .config/nvim/lua/custom/plugins/flash.lua
+++ .config/nvim/lua/custom/plugins/flash.lua
@@ -1,8 +1,14 @@
 return {
-	'folke/flash.nvim',
-	event = 'VeryLazy',
-	---@type Flash.Config
-	opts = {},
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      char = {
+        keys = { 'f', 'F', 't', 'T' }, -- disable ; and ,
+      },
+    },
+  },
 	-- stylua: ignore
 	keys = {
 		{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
